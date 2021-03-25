c16d8f974e44b3f0f911a878e7589eb8ec0f9ac2
OmegaLn <mohamedlaanadi1999@gmail.com>
2021-03-25 13:08:02 +0100

    tests burger && user V1

---
+++ back/spec/factories/burger.js
@@ -0,0 +1,13 @@
+const factoryGirl = require('factory-girl')
+const adapter = new factoryGirl.SequelizeAdapter()
+factory = factoryGirl.factory
+factory.setAdapter(adapter)
+
+const Burger = require('../../models').Burger
+
+factory.define('burger', Burger, {
+    title: factory.chance('word'),
+    description: factory.chance('sentence'),
+    price: factory.chance('floating', { min: 5, max: 15, fixed: 1 }),
+    image: factory.sequence((n) => `image${n}`)
+})
---
+++ back/spec/helpers/cleanDb.js
@@ -0,0 +1,5 @@
+const cleanDb = async(db) => {
+    await db.Burger.destroy({ truncate: { cascade: true } });
+    await db.Users.destroy({ truncate: { cascade: true } });
+}
+module.exports = cleanDb
