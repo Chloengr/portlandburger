502c848e09855cf157681922b7d427b745142057
OmegaLn <mohamedlaanadi1999@gmail.com>
2021-04-01 10:20:50 +0200

    changement panier à cart migration folder

---
+++ back/migrations/20210319125644-add-cart-id-to-cart_burger.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.addColumn('Cart_Burgers', 'CartId', {
+            type: Sequelize.INTEGER,
+            references: {
+                model: 'Carts', // name of Target model
+                key: 'id', // key in Target model that we're referencing
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL',
+        })
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.removeColumn('Cart_Burgers', 'CartId')
+    }
+};
